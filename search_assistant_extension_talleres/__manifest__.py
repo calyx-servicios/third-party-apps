3be769c98e098667f24cddf2f425142f02b08fde
Milton Guzman <miltonguzmanf@gmail.com>
2021-01-28 13:55:23 -0300

    [ADD][search_assistant_extension_talleres] Module extension Talleres

---
+++ search_assistant_extension_talleres/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Search Assistant Extension Talleres',
+    'summary': """
+        Extension of the module 'search_assistant' to adapt it to the client 'Talleres-Sustentables'
+    """,
+
+    'author': 'Calyx Servicios S.A., Odoo Community Association (OCA)',
+    'maintainers': ['miltonguzmanf@gmail.com'],
+
+    'website': 'http://odoo.calyx-cloud.com.ar/',
+    'license': 'AGPL-3',
+
+    'category': 'Technical Settings',
+    'version': '11.0.1.0.0',
+
+    'application': False,
+    'installable': True,
+
+    'depends': ['search_assistant'],
+
+}
