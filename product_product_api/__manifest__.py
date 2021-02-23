949b1092ec2be964212d92c95869c29179416aca
JhoneM <jhone0901@gmail.com>
2021-02-23 17:58:49 -0300

    [ADD][product_product_api] Incidence nbr./s.: ODOO-1094:  - New module to connect with odoo api rest.

---
+++ product_product_api/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Api",
+    "summary": """
+        Api Rest to make request to product.product""",
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["JhoneM"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Product",
+    "version": "11.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [],
+}
