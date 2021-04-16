fcf0c196a3e9780b858e6e54c882db9cb53cf744
paradisocrist <crisparadiso_@hotmail.com>
2021-04-16 10:58:21 -0300

    create group test

---
+++ test_test/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'HR Gamification custom',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'depends': ['hr_gamification','gamification'],
+    'description': """  """,
+    'data': [
+        'security/test.xml',
+    ],
+    'auto_install': True,
+
+}
