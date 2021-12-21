5ad1caedc0421d8a4bbe94f559107133192e32b3
garaceliguzman <georginaguzman267@gmail.com>
2021-12-21 15:01:19 -0300

    [FIX][product_technical_website]

--- product_technical_sheet/controller/main.py
+++ product_technical_sheet/controller/main.py
@@ -3,14 +3,17 @@
 import base64
 from werkzeug.utils import redirect
 import io
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 
 class AttachTechnical(http.Controller):
 
-    @http.route(['/features_technical_download'], type='http', auth='public')
-    def features_technical_download(self):
+    @http.route(['/features_technical_download/<model(product.template):product>'], type='http', auth='public')
+    def features_technical_download(self, product):
         
-        term_condition = request.env['product.template'].search([('website_default','=', True),('id','=', self.id)])
-        data = io.BytesIO(base64.standard_b64decode(term_condition.pdf_tyc))
-        return http.send_file(data, filename='fichatecnica.pdf', as_attachment=False)
+        term_condition = request.env['product.template'].search([('website_default','=', True), ('id', '=', product.id)])
+        if term_condition:
+            data = io.BytesIO(base64.standard_b64decode(term_condition.pdf_tyc))
+            return http.send_file(data, filename='fichatecnica.pdf', as_attachment=False)
+        else:
+            return "Este producto no posee una Ficha Tecnica"
