f0c55becaac6a3529f017a082e55ad1e81f0492b
garaceliguzman <georginaguzman267@gmail.com>
2021-12-21 11:50:32 -0300

    [][]

--- product_technical_sheet/controller/main.py
+++ product_technical_sheet/controller/main.py
@@ -11,6 +11,6 @@ class AttachTechnical(http.Controller):
     @http.route(['/features_technical_download'], type='http', auth='public')
     def features_technical_download(self):
         
-        term_condition = request.env['product.template'].search([('website_default','=', True)])
+        term_condition = request.env['product.template'].search([('website_default','=', True),('id','=', self.id)])
         data = io.BytesIO(base64.standard_b64decode(term_condition.pdf_tyc))
         return http.send_file(data, filename='fichatecnica.pdf', as_attachment=False)
