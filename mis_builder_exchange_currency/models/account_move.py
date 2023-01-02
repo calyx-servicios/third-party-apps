ae6907422e0de15d02098c3575842786909c3d89
gpperez <gpperez@gmail.com>
2023-01-02 17:52:01 -0300

    [UPD][mis_builder_exchange_currency] Incidence nbr./s.: ODOO-4905 Miss builder- Mejoras en hipervinculo del detalla de los apuntes contables   - Actualizado seteo de rates x mes Incidence nbr./s.: ODOO-4904 Mejoras - Miss builder - Multimonedas   - Actualizado seleccion variable de moneda en detalles (segun moneda definida en el reporte) Incidence nbr./s.: ODOO-5594 - Previsualizar reporte miss builder   - Corregido error al previsualizar reporte con multicompanias

--- mis_builder_exchange_currency/models/account_move.py
+++ mis_builder_exchange_currency/models/account_move.py
@@ -8,8 +8,10 @@ class AccountMoveLine(models.Model):
 
     def _get_exchange_currency(self):
         for move in self:
-            ec_id = self.env.context.get('exchange_currency_id')
-            move.exchange_currency_id = ec_id or 171 #MEP (se necesita recuperar la moneda definida en el reporte)
+            #se necesita recuperar la moneda definida en el reporte en curso (seteada en la configuracion dinamica de filtros del reporte)
+            report_currency_id = int(self.env['ir.config_parameter'].sudo().get_param('report_exchange_currency_id'))
+            ##############################################################
+            move.exchange_currency_id = report_currency_id or move.company_id.currency_id.id
 
     @api.depends('exchange_currency_id')
     def _compute_exchange_amount(self):
@@ -17,4 +19,6 @@ class AccountMoveLine(models.Model):
             rate = self.env['res.currency.rate'].search([('currency_id','=',move.exchange_currency_id.id),('name','<=',move.date)],order='name desc',limit=1)
             if rate:
                 move.exchange_amount = move.balance * rate[0].rate
+            else:
+                move.exchange_amount = move.balance
 
