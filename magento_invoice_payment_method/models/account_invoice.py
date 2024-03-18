b56306f4c5cbc5048c7d9812c030f4f561f5e241
enzogonzalezdev <enzogonzal@gmail.com>
2024-03-18 18:00:22 -0300

    [ODOO-6981][FIX][magento_invoice_payment_method]

--- magento_invoice_payment_method/models/account_invoice.py
+++ magento_invoice_payment_method/models/account_invoice.py
@@ -11,12 +11,10 @@ class AccountMove(models.Model):
     def _compute_magento_payment_method(self):
         magento_payment_method_name = ''
 
-        sale_orders = self.env['sale.order']._search_invoice_ids('in',{self.id})
-        if sale_orders:
+        sale_orders = self.env['sale.order']._search_invoice_ids('in',[self.id])
+        if sale_orders and not sale_orders[0][2] == []:
             sale_order = self.env['sale.order'].search([('id','=',sale_orders[0][2][0])])
             if sale_order:
                 magento_payment_method_name = sale_order[0].payment_method.name
 
         self.magento_payment_method = magento_payment_method_name
-
-
